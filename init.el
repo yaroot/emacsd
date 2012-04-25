f9701e528c2247dd55572f3f6498fc154f70f292
yaroot <yaroot@gmail.com>
2012-04-25 14:49:40 +0800

    .

--- init.el
+++ init.el
@@ -1,3 +1,14 @@
+
+(require 'package)
+(add-to-list 'package-archives
+  '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+
+(require 'evil)
+(evil-mode 1)
+
+
 ;;         (add-to-list 'load-path "~/.emacs.d")
 ;;         (add-to-list 'load-path "~/.emacs.d/vendor")
 ;;         
