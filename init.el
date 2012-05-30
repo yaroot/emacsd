e2082a182ac28e43852de83162b482cdf6b2b436
yaroot <yaroot@gmail.com>
2012-05-31 07:13:15 +0800

    add po-mode

--- init.el
+++ init.el
@@ -66,6 +66,16 @@
 ;(ido-mode t)
 
 
+(autoload 'po-mode "po-mode"
+          "Major mode for translators to edit PO files" t)
+(setq auto-mode-alist (cons '("\\.po\\'\\|\\.po\\." . po-mode)
+                            auto-mode-alist))
+
+(autoload 'po-find-file-coding-system "po-compat")
+(modify-coding-system-alist 'file "\\.po\\'\\|\\.po\\."
+                            'po-find-file-coding-system)
+
+
 ;(require 'rinari)
 
 
